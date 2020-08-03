cfc7829586cb9662d5945dd36185c46054411736
BlackSou <taras.iva96@gmail.com>
2020-08-04 02:44:36 +0300

    create validation rules

---
+++ app/Http/Requests/PlayRequest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Validation\Rule;
+class PlayRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(Request $request)
+    {
+      $rules = [
+          'title' => 'required|string|unique:plays,title',
+          'description' => '',
+          'complexity' => 'required|min:1|max:10',
+          'minPlayers' => 'required|min:1|max:10',
+          'maxPlayers' => 'required|min:1|max:10',
+          'isActive' => 'required|boolean'
+      ];
+
+      switch ($this->getMethod())
+      {
+        case 'POST':
+          return $rules;
+        case 'PUT':
+          return [
+            'play_id' => 'required|integer|exists:plays,id',
+            'title' => [
+              'required',
+              Rule::unique('plays')->ignore($this->title, 'title')
+            ]
+          ] + $rules;
+        case 'DELETE':
+          return [
+              'play_id' => 'required|integer|exists:plays,id'
+          ];
+      }
+    }
+}
